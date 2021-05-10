124b65ba36eb42e9b691dbd18edad60665697e19
m.ali <m.ali@elnusa.co.id>
2021-05-10 19:45:10 +0700

    day 2

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
 const http = require('http')
 const url = require('url')
-const { add }  = require('./src/modules/penjumlahan')
-const mysql = require('mysql')
-
-// const server = http.createServer((req,res) => {    
-//     const connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: '',
-//         port: '3306',
-//         database: 'inventory'
-//     })
-//     connection.connect((error, status)=> {
-//     if(error){
-//         throw error
-//     }
-//     console.log('connecting to database')
-//     });
-//     const uri = new url.parse(req.url, true)
-//     switch(uri.pathname){
-//         case '/':  
-//             const sql =  (`CREATE TABLE if not exists items(
-//                 id INT PRIMARY KEY auto_increment,
-//                 name VARCHAR(255)not null,
-//                 description VARCHAR(255),
-//                 available TINYINT(1) not null default 0
-//             )`)
-//             res.setHeader('content-type','application/json') 
-//             res.write(JSON.stringify({name: 'hello'}))
-//             connection.query(sql, (error, data) => {
-//                 if(error){
-//                     throw error
-//                 }
-//                 console.log('table created')
-//             })
-//             res
-//             break
-//         case '/open' :
-//             res.setHeader('content-type','application/json')
-//             res.write(JSON.stringify({}))
-//             break
-//     }
-//     res.end()
-// })
-// const PORT = process.env.port || 5000
-// server.listen(PORT, () => {
-//     console.log(`listen to port ${PORT}`)
-// })
 
 const server = http.createServer((req, res) => {        
     const uri = url.parse(req.url,true)    
