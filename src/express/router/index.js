6023b8d417ae41674ad6536964efb6606e312b24
githubmem <indra_eva2002@yahoo.com>
2021-06-07 19:48:46 +0700

    adding express routes

--- src/express/router/index.js
+++ src/express/router/index.js
@@ -1,14 +1,36 @@
-const express = require('express')
-const { addItem  }= require('../controller/add-items')
-const app = express()
+const express = require("express");
+const { addImages } = require("../controller/add-images");
+const { addItem } = require("../controller/add-items");
+const { deleteItem } = require("../controller/delete-items");
+const { getAllItems } = require("../controller/get-all-items");
+const { getItem } = require("../controller/get-item");
+const { updateItem } = require("../controller/update-items");
 
-// router 
-app.post('/tambah-item', (req, res) => { addItem(req, res) })
-app.get('/tambah-item', (req, res) => { addItem(req, res) })
+const app = express();
 
-const PORT = process.env.PORT || 5001
+// router
+app.post("/tambah-image", (req, res) => {
+  addImages(req, res);
+});
+app.post("/tambah-item", (req, res) => {
+  addItem(req, res);
+});
+app.delete("/hapus-item", (req, res) => {
+  deleteItem(req, res);
+});
+app.get("/tampil-semua-items", (req, res) => {
+  getAllItems(req, res);
+});
+app.get("/tampil-item", (req, res) => {
+  getItem(req, res);
+});
+app.put("/ubah-item", (req, res) => {
+  updateItem(req, res);
+});
+
+const PORT = process.env.PORT || 5001;
 const server = app.listen(PORT, () => {
-    console.log(`listening to port ${PORT}`)
-})
+  console.log(`listening to port ${PORT}`);
+});
 
-module.exports = {server}
+module.exports = { server };
